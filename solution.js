58fdb27856ca9bb64e565a055140f5a357064961
Rami B <ramibasheer9@gmail.com>
2022-06-30 17:28:48 +0200

    submitting my answers

--- solution.js
+++ solution.js
@@ -1,1 +1,67 @@
 // write your code in this file
+const total = 6450;
+const cost = 15;
+
+console.log('Q1:', total / cost, 'Tickets are sold.')
+
+
+const income = 500;
+
+console.log('Q2:', income * 52,1429, 'dollar Sylvia makes a year')
+
+
+// console.log('Q3:', [17 / 30 * 100], '%')
+const calc = (17 / 30) * 100;
+console.log(`Q3: ${calc}%`);
+
+let side = 4.75;
+console.log(`Q4: ${side * 4}cm is the perimeter of the sqaure`)
+
+let side1 = 3;
+let side2 = 4;
+let side3 = 5;
+console.log(`Q5: ${side1 + side2 + side3}cm is the perimeter of the triangle`)
+
+
+let squarSide1 = 5;
+let squarSide2 = 5;
+console.log(`Q6: ${squarSide1 * squarSide2}cm² is the sqaure area`)
+
+
+let triangleSide1 = 5;
+let triangleSide2 = 4;
+let triangleSide3 = 3;
+
+console.log(`Q7: ${triangleSide2 * triangleSide3 / 2} is the area of this right-angled triangle`)
+
+
+let length = 9;
+console.log(`Q8: ${length * length * length} is the colume of the cube`)
+
+
+
+let bill1 = 22.35;
+let tip1 = 10 / 100
+
+let bill2 = 26.67;
+let tip2 = 15 / 100
+
+let bill3 = 35.92;
+let tip3 = 20 / 100
+
+console.log(`Q9: first bill is ${tip1 * bill1}€ second bill is ${tip2*bill2}€ third bill is ${tip3 * bill3}€`)
+
+console.log(`Q10: ${(8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4)/10} hours is the avrage worked hour per day`)
+
+const fiveTests = [75, 70, 85, 90, 100]
+const fiveTestsTotal = fiveTests[0] + fiveTests[1] + fiveTests[2] + fiveTests[3] + fiveTests[4];
+const sixTestsTotal = 85 * 6
+console.log(`Q11: Score in the sixth test: ${sixTestsTotal - fiveTestsTotal}`);
+
+const currentAverage = 78;
+const wantedAverage = 80;
+ console.log(`Q12: James needs a minimum of ${wantedAverage * 2 - currentAverage}% to get an 80% average`)
+
+
+
+
